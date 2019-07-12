a4cc39df1007dc3e7be58d90875995fa1419355d
Max <max.schwanger@gmail.com>
2019-07-11 22:17:54 -0400

    end of video 9 - show component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Show from './Show';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tv = [
+  {
+    id: 1,
+    name: 'The Real Housewives'
+  },
+  {
+    id: 2,
+    name: 'Below Deck'
+  },
+  {
+    id: 3,
+    name: 'Big Brother'
+  }
+];
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>LUTV</h1>
+        </header>
+        {tv.map(show => (
+          <Show key={show.id} show={show} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
